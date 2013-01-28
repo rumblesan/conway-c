a490b6e4d06cd7edbee6e712b2b83d5e17ccaaf6
Guy John <guy@rumblesan.com>
2013-01-28 12:38:39 +0000

    Adding basic grid iteration tests to test suite

--- src/tests/test_gol.c
+++ src/tests/test_gol.c
@@ -14,8 +14,36 @@ static char * test_creation() {
     return 0;
 }
 
+static char * test_iterate_creation() {
+    GOL g = gol_create_grid(10, 10);
+
+    GOL new_g = gol_iterate_grid(g);
+
+    mu_assert("Error: iterated grid should be a new grid", g != new_g);
+
+    gol_cleanup_grid(new_g);
+    return 0;
+}
+
+static char * test_iterate_all_zeros() {
+    GOL g = gol_create_grid(10, 10);
+
+    GOL new_g = gol_iterate_grid(g);
+
+    for (int x = 0; x < new_g->cells_x; x++) {
+        for (int y = 0; y < new_g->cells_y; y++) {
+            mu_assert("Error: grid coord should be 0", new_g->grid[x][y] == 0);
+        }
+    }
+
+    gol_cleanup_grid(new_g);
+    return 0;
+}
+
 char * test_gol() {
     mu_run_test(test_creation);
+    mu_run_test(test_iterate_creation);
+    mu_run_test(test_iterate_all_zeros);
 
     return 0;
 }
