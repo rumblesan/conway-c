ac10592d4e930e03b2fce8b4344d14eb39a30380
Guy John <guy@rumblesan.com>
2013-02-02 14:04:57 +0000

    Adding functions to get colour components

--- include/core/colour.h
+++ include/core/colour.h
@@ -7,4 +7,8 @@ typedef uint32_t colour;
 
 colour colour_create(uint8_t r, uint8_t g, uint8_t b);
 
+uint8_t colour_get_red(colour c);
+uint8_t colour_get_green(colour c);
+uint8_t colour_get_blue(colour c);
+
 #endif
--- src/core/colour.c
+++ src/core/colour.c
@@ -6,3 +6,15 @@ uint32_t colour_create(uint8_t r, uint8_t g, uint8_t b) {
     return (r << 16) + (g << 8) + (b);
 }
 
+uint8_t colour_get_red(colour c) {
+    return (c >> 16) & 255;
+}
+
+uint8_t colour_get_green(colour c) {
+    return (c >> 8) & 255;
+}
+
+uint8_t colour_get_blue(colour c) {
+    return (c >> 0) & 255;
+}
+
